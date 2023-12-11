d4f55954b0942ab04689dccdd91b5ad8ec9ce31f
alexandrabakhar <112278780+alexandrabakhar@users.noreply.github.com>
2023-12-11 21:21:16 +0300

    feat: add character

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,18 +1,23 @@
 import { Col } from "antd";
 import * as S from "./style.ts";
 export const Main = () => {
-	
 	return (
 		<S.Container>
-			<Col span={2}/>
+			<Col span={2} />
 			<S.TextContainer span={11}>
-			<S.MailTitle>CHATBOT FDSAFDJ DSADAS</S.MailTitle>
-		<S.SubTitle level={2}>sdjksakjdbbdhsbfhjdsadksnkdsksalkdfbhjdsbfhjbjdhbhjgsbhjbashjfbddsjhbfhjdsbfhjasbhdsbfhjdbsfjhbadhsbdhsjbfsdbfjhbsdhfbdshjfbdsfhbdshjbfhdsbfhjksdbfkhadsb</S.SubTitle>
+				<S.MailTitle>CHATBOT FDSAFDJ DSADAS</S.MailTitle>
+				<S.SubTitle level={2}>
+					sdjksakjdbbdhsbfhjdsadksnkdsksalkdfbhjdsbfhjbjdhbhjgsbhjbashjfbddsjhbfhjdsbfhjasbhdsbfhjdbsfjhbadhsbdhsjbfsdbfjhbsdhfbdshjfbdsfhbdshjbfhdsbfhjksdbfkhadsb
+				</S.SubTitle>
 			</S.TextContainer>
-      <S.ImgBlock span={11}>
-				<S.Img preview={false} width={'25vw'} height={'25vw'} src="/public/main.png"/>
-				</S.ImgBlock>
-			
-    </S.Container>
+			<S.ImgBlock span={11}>
+				<S.Img
+					preview={false}
+					width={"25vw"}
+					height={"25vw"}
+					src="/public/character.webp"
+				/>
+			</S.ImgBlock>
+		</S.Container>
 	);
 };
