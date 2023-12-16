1e16b2a32533edfa8c42d9ab5c787b9fa1c5f1af
Роман Кондрашев <distanceon@MacBook-Air-Roman.local>
2023-12-16 08:27:02 +0300

    feat: create icons for functions

--- src/components/Functions/Functions.tsx
+++ src/components/Functions/Functions.tsx
@@ -1,19 +1,20 @@
 import * as S from "./style";
+import {SearchOutlined, DatabaseOutlined, CustomerServiceOutlined} from '@ant-design/icons';
 
 export const Functions = () => {
 	const items = [
 		{
-			icon: "",
+			icon: () => <SearchOutlined />,
 			title: "Глубокое понимание",
 			desc: "Сириус точно интерпретирует ваши вопросы и предоставляет наиболее релевантные ответы",
 		},
 		{
-			icon: "",
+			icon: () => <DatabaseOutlined/>,
 			title: "Обучение на данных",
 			desc: "С каждым запросом Сириус становится умнее, оптимизируя свои ответы и предложения для повышения точности и полезности",
 		},
 		{
-			icon: "",
+			icon: () => <CustomerServiceOutlined/>,
 			title: "Комплексная поддержка",
 			desc: "Если система не найдет готовый ответ, она поможет сформировать обращение к сотруднику администрации и обеспечит вас обратной связью",
 		},
@@ -26,7 +27,7 @@ export const Functions = () => {
 				{items.map((item, key) => (
 					<div key={key}>
 						<S.Item >
-							<S.ItemIcon />
+							<S.ItemIcon>{item.icon()}</S.ItemIcon>
 							<S.ItemText>
 								<S.Subtitle>{item.title}</S.Subtitle>
 								<S.Desc>{item.desc}</S.Desc>
